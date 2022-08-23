912114150b263bca864ae0009bbf246ad0ef4bd2
Huasito-Appel <sergiomatamala@MacBook-Air-de-mac.local>
2022-08-23 10:47:35 -0400

    Bug(fixed) fixed bug when they always Tie, not sure if the worfklow of spot is corret tho

--- Cachipun/rpsls.py
+++ Cachipun/rpsls.py
@@ -3,7 +3,11 @@ import random
 user = int(input("0:Piedra, 1:Papel, 2:Tijeras, 3:Lagarto, 4:Spock "))
 comp = random.randint(0,4)
 
-if user != comp:
+while user != 0 and user != 1 and user != 2 and user != 3 and user != 4:
+    print("Please follow the input format, select a number to play!")
+    user = int(input("0:Piedra, 1:Papel, 2:Tijeras, 3:Lagarto, 4:Spock "))
+
+if user == comp:
     print("User",user,"Comp",comp,"->","EMPATE")
 
 elif (((user+1) % 5) == comp) or (((user+3) % 5) == comp):
